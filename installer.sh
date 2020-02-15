f22e4cb51c9516eef58b6caf1c82cecc84b865c1
Danilo Yamashita <dhiroshi@ciandt.com>
2020-02-15 16:49:23 -0300

    install dependencies

--- installer.sh
+++ installer.sh
@@ -2,32 +2,40 @@
 
 if ! [ -x "$(command -v git)" ]; then
   echo -e '\e[41m>> Error: git not installed.\e[49m' >&2
-  exit 1
+  echo 'Installing git...'
+  sudo apt-get install git.
 fi
 
 if ! [ -x "$(command -v vim)" ]; then
   echo -e '\e[41m>> Error: vim 8 not installed.\e[49m' >&2
-  exit 1
+  echo 'Installing vim...'
+  sudo add-apt-repository ppa:jonathonf/vim
+  sudo apt update
+  sudo apt install vim
 fi
 
 if ! [ -x "$(command -v gvim)" ]; then
   echo -e '\e[41m>> Error: gvim 8 not installed.\e[49m' >&2
-  exit 1
+  echo 'Installing gvim...'
+  sudo apt-get install vim-gnome
 fi
 
 if ! [ -x "$(command -v composer)" ]; then
   echo -e '\e[41m>> Error: composer not installed.\e[49m' >&2
-  exit 1
+  echo 'Installing composer...'
+  sudo apt install composer
 fi
 
 if ! [ -x "$(command -v ag)" ]; then
   echo -e '\e[41m>> Error: ag not installed.\e[49m' >&2
-  exit 1
+  echo 'Installing ag...'
+  sudo apt install silversearcher-ag
 fi
 
 if ! [ -x "$(command -v ctags)" ]; then
   echo -e '\e[41m>> Error: ctags not installed.\e[49m' >&2
-  exit 1
+  echo 'Installing ctags...'
+  sudo apt install exuberant-ctags
 fi
 
 echo 'Install vimrc'
